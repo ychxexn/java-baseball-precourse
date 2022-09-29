e5d1a693012b8c0aea13e87a400611e187894f3f
CHEESE <81912261+ychxexn@users.noreply.github.com>
2022-09-29 21:17:14 +0900

    refactor : 오타 수정

--- src/main/java/baseball/util/Message.java
+++ src/main/java/baseball/util/Message.java
@@ -3,9 +3,9 @@ package baseball.util;
 public enum Message {
     INPUT("숫자를 입력해주세요 : "),
     CHOICE("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."),
-    BALL("%d 볼 "),
-    STRIKE("%d 스트라이크"),
-    NOTHING("나싱"),
+    BALL("%d볼 "),
+    STRIKE("%d스트라이크"),
+    NOTHING("낫싱"),
     FINISH("3개의 숫자를 모두 맞히셨습니다! 게임 종료"),
     ERROR_INPUT("잘못된 입력입니다. 프로그램을 종료합니다.");
 
